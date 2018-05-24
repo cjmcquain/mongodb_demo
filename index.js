0e3dc1c4d181727e15507ec7c2253743af680000
Charles McQuain <charlesmcquain@gmail.com>
2018-05-23 21:58:54 -0700

    Created course Schema

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost");
+mongoose
+  .connect("mongodb://localhost")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.log("Could not connect to MongoDB..."));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
