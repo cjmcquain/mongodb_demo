52195b4c1c5e4d7c8ee346af19db4cfa063a27b2
Charles McQuain <charlesmcquain@gmail.com>
2018-05-24 22:18:29 -0700

    Added query to filter courses through DB

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find();
+  const courses = await Course.find({ author: "Charles", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
   console.log(courses);
 }
 
